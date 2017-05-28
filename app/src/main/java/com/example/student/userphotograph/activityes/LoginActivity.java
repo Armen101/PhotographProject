0dfc59fc5c331576b12c7123d3ead0f7136490d3
Armen Gevorgyan <gevorgyanweb@gmail.com>
2017-05-28 22:40:06 +0400

    added initial LoginActivity and fragments

---
+++ app/src/main/java/com/example/student/userphotograph/activityes/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.student.userphotograph.activityes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.student.userphotograph.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
