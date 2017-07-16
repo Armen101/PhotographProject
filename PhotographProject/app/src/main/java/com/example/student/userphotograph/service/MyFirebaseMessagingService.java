9a5280ec04724f92e71c423fa78f92736fee3572
Babken <babkenmkhitaryan@gmail.com>
2017-07-16 15:15:23 +0400

    Updated firebase messaging service to push back notification.

--- PhotographProject/app/src/main/java/com/example/student/userphotograph/service/MyFirebaseMessagingService.java
+++ PhotographProject/app/src/main/java/com/example/student/userphotograph/service/MyFirebaseMessagingService.java
@@ -16,15 +16,13 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    private String clientToken;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        clientToken = remoteMessage.getData().get("token");
-        sendNotification();
+        sendNotification(remoteMessage.getData().get("token"),(remoteMessage.getData().get("title")));
     }
 
-    private void sendNotification() {
+    private void sendNotification(String token, String title) {
         Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder mBuilder =
                 (NotificationCompat.Builder) new NotificationCompat.Builder(this)
@@ -36,17 +34,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent resultPendingIntent = PendingIntent.getActivity(
                 this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.setContentText("You have a new order!");
+        mBuilder.setContentTitle(title);
         mBuilder.setSound(uri);
         mBuilder.setPriority(Notification.PRIORITY_MAX);
-        Intent notifyIntent =
+
+        Intent notifyIntentAccept =
                 new Intent(this, NotificationActionService.class);
-        notifyIntent.putExtra("Token", clientToken);
-        notifyIntent.setAction(NotificationActionService.ACTION_ACCEPT);
-        notifyIntent.setAction(NotificationActionService.ACTION_REJECT);
-        PendingIntent notif = PendingIntent.getService(this,0,notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.addAction(R.drawable.ic_accept, "Accept", notif);
-        mBuilder.addAction(R.drawable.ic_reject, "Reject", notif);
+        notifyIntentAccept.putExtra("Token", token);
+        notifyIntentAccept.setAction(NotificationActionService.ACTION_ACCEPT);
+        PendingIntent accept = PendingIntent.getService(this,0,notifyIntentAccept, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent notifyIntentReject =
+                new Intent(this, NotificationActionService.class);
+        notifyIntentReject.putExtra("Token", token);
+        notifyIntentReject.putExtra("Title", title);
+        notifyIntentReject.setAction(NotificationActionService.ACTION_REJECT);
+        PendingIntent pendingIntent = PendingIntent.getService(this,0,notifyIntentReject, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        mBuilder.addAction(R.drawable.ic_accept, "Accept", accept);
+        mBuilder.addAction(R.drawable.ic_reject, "Reject", pendingIntent);
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(0, mBuilder.build());
--- PhotographProject/app/src/main/java/com/example/student/userphotograph/service/NotificationActionService.java
+++ PhotographProject/app/src/main/java/com/example/student/userphotograph/service/NotificationActionService.java
@@ -6,15 +6,8 @@ import android.content.Intent;
 import com.example.student.userphotograph.utilityes.NetworkHelper;
 
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions and extra parameters.
- */
 public class NotificationActionService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+
     public static final String ACTION_ACCEPT = "Accept";
     public static final String ACTION_REJECT = "Reject";
 
@@ -28,7 +21,6 @@ public class NotificationActionService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             final String clientToken = intent.getStringExtra("Token");
-            System.out.println("clientToken =======================::: Action service " + clientToken);
             if (ACTION_ACCEPT.equals(action)) {
                 NetworkHelper.sendNotificationRequest(clientToken, "OK");
             } else if (ACTION_REJECT.equals(action)) {
